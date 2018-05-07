75f012014cf44566e75dda9d0bc087d2a393fec2
RolfJurgens <muwolf@gmail.com>
2018-05-07 12:46:34 +0200

    HW update

--- HW_Edwin/WifiShield/WifiShield.ino
+++ HW_Edwin/WifiShield/WifiShield.ino
@@ -1,51 +1,50 @@
 
 #include <SoftwareSerial.h>
 #include <SparkFunESP8266WiFi.h>
-#define NETWORK_NAME "network"
-#define NETWORK_PASSWORD "password"
+#define NETWORK_NAME "AndroidAP"
+#define NETWORK_PASSWORD "fwxp3871"
+
+
 
 void setup() {
+  setupESP8266();
 
-  Serial.begin(9600);
+  /*
+    Serial.begin(9600);
+    int succes = esp8266.begin(9600);
+    if (succes) {
+      Serial.println("ESP8622 Shield found");
+    } else {
+      Serial.println("geen contact met ESP8266");
+      while (1);
+    }
 
-  int succes = esp8266.begin(9600);
-  if (succes) {
-    Serial.println("ESP8622 Shield found");
-  } else {
-    Serial.println("geen contact met ESP8266");
-    while (1);
-  }
+    Serial.print("Trying to connect to: ");
+    Serial.print(NETWORK_NAME);
+    Serial.print(" with password: ");
+    Serial.println(NETWORK_PASSWORD);
+    int result = esp8266.connect(NETWORK_NAME, NETWORK_PASSWORD);
+    if (result < 0) {
+      Serial.print("ERROR connecting: ");
+      Serial.print(result);
+      while (1);
+    }
 
-  Serial.print("Trying to connect to: ");
-  Serial.print(NETWORK_NAME);
-  Serial.print(" with password: ");
-  Serial.println(NETWORK_PASSWORD);
-  int result = esp8266.connect(NETWORK_NAME, NETWORK_PASSWORD);
-  if (result < 0) {
-    Serial.print("ERROR connecting: ");
-    Serial.print(result);
-    while (1);
-  }
+    IPAddress myIP = esp8266.localIP();
+    Serial.print("My IP: ");
+    Serial.println(myIP);
 
-  IPAddress myIP = esp8266.localIP();
-  Serial.print("My IP: ");
-  Serial.println(myIP);
-
-  int wifiConnected = esp8266.status();
+    int wifiConnected = esp8266.status();*/
 }
 
 void loop() {
 
+  ESP8266Client client;
+  client.connect("km4.mobiapt.nl", 80);
 
-  /*  ESP8622Client client;
-    client.connect("km4.mobiapt.nl", 80);
 
-    //Versturen;
-    client.print("verstuur");
-    //Ontvangen;
-    while (client.available())
-      Serial.print(client.read());
+  client.print("km4.mobidapt.com");
 
-  */
-
+  while (client.available())
+    Serial.print(client.read());
 }
---
+++ HW_Edwin/httprequest/httprequest.ino
@@ -0,0 +1,49 @@
+#include <SoftwareSerial.h>
+#include <SparkFunESP8266WiFi.h>
+#define NETWORK_NAME "AndroidAP"
+#define NETWORK_PASSWORD "fwxp3871"
+
+bool colour = true;
+
+void setup() {
+  setupESP8266();
+}
+
+void loop() {
+
+  ESP8266Client client;
+  int result = client.connect("km4.mobidapt.com", 80);
+  if (result <= 0) {
+    Serial.println("Failed to connect to server");
+    delay(1000);
+  } else {
+
+    Serial.println("Sent HTTP request");
+
+    if (colour) {
+      client.println("GET /lights?userid=rolf&lightid=light1&colour=green HTTP/1.1\n"
+                     "Host: km4.mobidapt.com\n"
+                     "Connection: close\n");
+      Serial.println("Green!");
+    }
+
+    if (!colour) {
+      client.println("GET /lights?userid=rolf&lightid=light1&colour=red HTTP/1.1\n"
+                     "Host: km4.mobidapt.com\n"
+                     "Connection: close\n");
+      Serial.println("Red!");
+    }
+
+    Serial.println("Responce from server: ");
+    while (client.available()) {
+      Serial.write(client.read());
+    }
+    Serial.print("Colour is: ");
+    Serial.println(colour);
+    colour = ! colour;
+    if (client.connected()) {
+      client.stop();
+    }
+  }
+  delay(4000);
+}
